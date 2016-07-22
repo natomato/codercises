3bf77647cdfc2feffb5e08049421912f99cd88d1
Nate Tate <nate91711@gmail.com>
2016-07-22 18:04:20 +0000

    adds recursiveSet

--- challenges/recursiveSet.js
+++ challenges/recursiveSet.js
@@ -1,6 +1,10 @@
-function add () {
-  console.log("hello!")
-  return 2+2
+// Write a recursive function for generating all permutations of an input string. Return them as a set.
+// Don't worry about time or space complexity—if we wanted efficiency we'd write an iterative version.
+// To start, assume every character in the input string is unique.
+// Your function can have loops—it just needs to also be recursive.
+
+function recursiveSet (str) {
+  console.log(str)
 }
 
-add()
+recursiveSet('hi')
